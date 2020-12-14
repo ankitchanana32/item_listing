130a5d734955f0d86543bbfa1823f83792d4bb13
ankit_chanana <ankitchanana32@gmail.com>
2020-12-15 00:31:59 +0530

    update product listing

--- src/Components/ProductList/index.jsx
+++ src/Components/ProductList/index.jsx
@@ -2,11 +2,30 @@ import ProductCard from "../ProductCard";
 import "./style.css";
 
 const ProductList = (props) => {
+  const extractVariants = (prod) => {
+    let prodvariants = {};
+    prod.Variant.forEach((variant) => {
+      prodvariants[variant.variationId] = {
+        prodId: prod.id,
+        name: prod.name,
+        brand: prod.brand,
+        variantId: variant.variationId,
+        cost: variant.cost,
+        color: variant.color,
+      };
+    });
+    return prodvariants;
+  };
   return (
     <>
       {props.products &&
         props.products.map((product, index) => {
-          return <ProductCard product={product} key={index}></ProductCard>;
+          return (
+            <ProductCard
+              product={extractVariants(product)}
+              key={index}
+            ></ProductCard>
+          );
         })}
     </>
   );
